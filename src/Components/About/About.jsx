6039dfff57e50b81b4e17a7e5c263ff1cf029679
kavetech2023 <kavetech@gmail.com>
2024-07-30 01:12:33 +0300

    feat: Add About component with HTML structure and CSS styles

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src="https://via.placeholder.com/150" alt="" />
+        </div>
+        <div className="about-sections">
+            <div className="about-left">
+                <img src="https://via.placeholder.com/150" alt="" />
+            </div>
+            <div className="about-right">
+                <div className="about-paragraph">
+                    <p>I  am an experienced front end bla bla bla</p>
+                    <p>My passion for frontend begun when</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill">
+                        <p>REACT JS</p><hr style={{width:"90%"}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>REACT JS</p><hr style={{width:"90%"}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>REACT JS</p><hr style={{width:"90%"}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>REACT JS</p><hr style={{width:"90%"}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>REACT JS</p><hr style={{width:"90%"}} />
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="about-achievements">
+                <div className="about-achievement">
+                    <h1>10+</h1>
+                    <p>Years of experience</p>
+                </div>
+                <hr />
+                <div className="about-achievement">
+                    <h1>90+</h1>
+                    <p>Projects completed.</p>
+                </div>
+                <hr />
+                <div className="about-achievement">
+                    <h1>15+</h1>
+                    <p>Happy clients</p>
+                </div>
+            </div>
+        </div>
+    
+  )
+}
+
+export default About
