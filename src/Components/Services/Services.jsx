45cb05ff0cd315b33fb224333c5e70c72434efda
kavetech2023 <kavetech@gmail.com>
2024-07-30 01:14:12 +0300

    chore: Add Services component with HTML structure and CSS styles

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './Services.css'
+import { MdOutlineNavigateNext } from "react-icons/md";
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className="services-title">
+            <h1>My Services</h1>
+            <img src="https://via.placeholder.com/150" alt="" />
+        </div>
+        <div className="services-container">
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+
+            <div className="services-format">
+                <h3>Service Number</h3>
+                <h2>Service name</h2>
+                <p>Service description</p>
+                <div className="services-readmore">
+                    <p>Read More</p>
+                    <span><MdOutlineNavigateNext /></span>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Services
