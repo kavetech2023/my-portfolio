4a9942188f9bdec38f5e8a206600a937da8b4cc9
kavetech2023 <kavetech@gmail.com>
2024-07-30 10:39:46 +0300

    refactor: Add lazy loading for Hero component images

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id="home" className='hero'>
         <img src="https://via.placeholder.com/150" alt="hero" />
         <h1><span>I'm Kevin Kavete,</span> a frontend developer based in USA.</h1>
         <p>I have 10+ years of experience in market manipulation.</p>
         <div className="hero-action">
             <div className="hero-connect">
-                Connect with me
+            <AnchorLink className='anchor-link' offset={50} href="#contact"><p onClick={()=>setMenu("contact")}>Connect with me</p></AnchorLink>
+                
             </div>
             <div className="hero-resume">
                 My Resume
