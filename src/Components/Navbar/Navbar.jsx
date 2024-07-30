e3465732eb1f6cbd625bdc83f06cb3166f54745a
kavetech2023 <kavetech@gmail.com>
2024-07-30 15:04:38 +0300

    refactor: Improve mobile responsiveness and accessibility of Navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
-import React ,{useState}from 'react'
+import React ,{useRef, useState}from 'react'
 import './Navbar.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("home")
+  const menuRef = useRef();
+
+  const openMenu = () => {  
+      menuRef.current.style.right = "0";
+  }
+  const closeMenu = () => {  
+    menuRef.current.style.right = "-350px";
+}
 
   return (
     <div className='navbar'>
         <span>Logo</span>
-        <ul className="nav-menu">
+        <span className='nav-mob-open' onClick={openMenu}><GiHamburgerMenu /></span>
+        <ul ref={menuRef} className="nav-menu">
+            <span className='nav-mob-close' onClick={closeMenu}><IoCloseSharp /></span>
             <li><AnchorLink className='anchor-link' offset={50} href="#home"><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<span>_</span>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href="#about"><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<span>_</span>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<span>_</span>:<></>}</li>
